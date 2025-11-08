c559dd14f487940b8687cb78c2337b2d1ea55053
. <fenik400@gmail.com>
2025-11-08 20:18:35 +0300

    init

---
+++ src/main/java/org/legion/simpleMail/SimpleMail.java
@@ -0,0 +1,17 @@
+package org.legion.simpleMail;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SimpleMail extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
